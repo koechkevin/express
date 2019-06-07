b72b98a4502e296c730ba2ebb89de2c64665298f
koechkevin <koechkevin92@gmail.com>
2019-06-07 20:47:53 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import http from 'http';
 import app from './app';
 
 const port = 3000;
 
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log('This is an express app');
+app.listen(port, () => {
+  console.log(`This is an express app port ${port}`);
 });
 
 
